6c4e891913567a200a3164fff0ca6a6f604e6741
David Lee <david@lee269.co.uk>
2023-04-22 19:11:33 +0100

    added acquire_mm23

---
+++ R/acquire_mm23.R
@@ -0,0 +1,29 @@
+#' Download a raw mm23.csv file
+#'
+#' @param path Folder to put the downloaded data in. If missing a tempfile will
+#'   be created.
+#'
+#' @return Path and filename of the downloaded file.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' file <- acquire_mm23()
+#' data <- file |> get_mm23_metadata()
+#'
+#' my_mm23 <- get_mm23("~/data")
+#' }
+acquire_mm23 <- function(path){
+    url <- "https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceindices/current/mm23.csv"
+
+  if(missing(path)){
+    tmp <- tempfile()
+    utils::download.file(url, tmp)
+    mm23file <- tmp
+  } else {
+    utils::download.file(url, destfile = paste0(path, "/", "mm23.csv"))
+    mm23file <- paste0(path, "/", "mm23.csv")
+  }
+
+    return(mm23file)
+}
