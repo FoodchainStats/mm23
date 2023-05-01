2d8e5448456a1b690125fe1f93f78a118fbe1fe9
David Lee <david@lee269.co.uk>
2023-05-01 18:22:32 +0100

    reftables url

--- R/helpers.R
+++ R/helpers.R
@@ -4,3 +4,12 @@ mm23_url <- function(){
   url <- "https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceindices/current/mm23.csv"
   return(url)
 }
+
+
+# Stores the link to the reference tables xlsx. Im not sure if this url is
+# stable or not.
+# Not exported
+reftables_url <- function(){
+  url <- "https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceinflation/current/consumerpriceinflationdetailedreferencetables2.xlsx"
+  return(url)
+}
