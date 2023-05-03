1ab1a7bf0d241e75e2040873a33364f69c341db9
David Lee <david@lee269.co.uk>
2023-05-03 20:39:46 +0100

    tentative unchain function

--- R/helpers.R
+++ R/helpers.R
@@ -13,3 +13,22 @@ reftables_url <- function(){
   url <- "https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceinflation/current/consumerpriceinflationdetailedreferencetables2.xlsx"
   return(url)
 }
+
+# Hopefully unchains a CPI series
+# not exported
+unchain <- function(month, value) {
+  case_when(
+    month == 1 ~ value/lag(value),
+    month == 2 ~ value/lag(value, 1),
+    month == 3 ~ value/lag(value, 2),
+    month == 4 ~ value/lag(value, 3),
+    month == 5 ~ value/lag(value, 4),
+    month == 6 ~ value/lag(value, 5),
+    month == 7 ~ value/lag(value, 6),
+    month == 8 ~ value/lag(value, 7),
+    month == 9 ~ value/lag(value, 8),
+    month == 10 ~ value/lag(value, 9),
+    month == 11 ~ value/lag(value, 10),
+    month == 12 ~ value/lag(value, 11)
+  )
+}
