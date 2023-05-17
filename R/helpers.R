d6f857589fc93e68debd4021b695d8e4e507d004
David Lee <david.lee@defra.gov.uk>
2023-05-17 08:58:07 +0100

    exporting some helper functions

--- R/helpers.R
+++ R/helpers.R
@@ -1,34 +1,55 @@
-# Stores the ONS url
-# Not exported
+#' Default url for the current mm23 dataset
+#'
+#' @return url of mme23 data
+#' @export
+#'
+#' @examples
+#' url <- mm23_url()
 mm23_url <- function(){
   url <- "https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceindices/current/mm23.csv"
   return(url)
 }
 
 
-# Stores the link to the reference tables xlsx. Im not sure if this url is
-# stable or not.
-# Not exported
+#' Default url for the CPI reference tables spreadsheet
+#'
+#' Its not clear yet if this url is stable, need to test over future months.
+#'
+#' @return url of reference tables
+#' @export
+#'
+#' @examples
+#' url <- reftables_url()
 reftables_url <- function(){
   url <- "https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceinflation/current/consumerpriceinflationdetailedreferencetables2.xlsx"
   return(url)
 }
 
-# Hopefully unchains a CPI series
-# not exported
+#' Hopefully unchains a CPI series
+#'
+#' Requires a data set with a month variable, uses dplyr::lag to unchain using
+#' previous values. Assumes that CPI weights change twice a year.
+#'
+#' @param month The month number for the index
+#' @param value The index value
+#'
+#' @return An unchained index value
+#' @export
+#'
+#' @examples
 unchain <- function(month, value) {
   dplyr::case_when(
-    month == 1 ~ value/lag(value) * 100,
-    month == 2 ~ value/lag(value, 1) * 100,
-    month == 3 ~ value/lag(value, 2) * 100,
-    month == 4 ~ value/lag(value, 3) * 100,
-    month == 5 ~ value/lag(value, 4) * 100,
-    month == 6 ~ value/lag(value, 5) * 100,
-    month == 7 ~ value/lag(value, 6) * 100,
-    month == 8 ~ value/lag(value, 7) * 100,
-    month == 9 ~ value/lag(value, 8) * 100,
-    month == 10 ~ value/lag(value, 9) * 100,
-    month == 11 ~ value/lag(value, 10) * 100,
-    month == 12 ~ value/lag(value, 11) * 100
+    month == 1 ~ value/dplyr::lag(value) * 100,
+    month == 2 ~ value/dplyr::lag(value, 1) * 100,
+    month == 3 ~ value/dplyr::lag(value, 2) * 100,
+    month == 4 ~ value/dplyr::lag(value, 3) * 100,
+    month == 5 ~ value/dplyr::lag(value, 4) * 100,
+    month == 6 ~ value/dplyr::lag(value, 5) * 100,
+    month == 7 ~ value/dplyr::lag(value, 6) * 100,
+    month == 8 ~ value/dplyr::lag(value, 7) * 100,
+    month == 9 ~ value/dplyr::lag(value, 8) * 100,
+    month == 10 ~ value/dplyr::lag(value, 9) * 100,
+    month == 11 ~ value/dplyr::lag(value, 10) * 100,
+    month == 12 ~ value/dplyr::lag(value, 11) * 100
   )
 }
