d63e3d8b76c3853be0606320f366b897e8fcf7e4
David Lee <david.lee@defra.gov.uk>
2023-07-26 14:43:35 +0100

    created generic get_weights function

--- R/weights.R
+++ R/weights.R
@@ -1,3 +1,135 @@
+# Extract the January weights from  the ONS detailed reference tables
+# spreadsheet. They are not included in mm23 which only has the weight at
+# annual level. Exclude certain cdids since they are either duplicated or do
+# not appear in the mm23 data
+get_weights_jan <- function(measure = "cpi") {
+
+  measure <- tolower(measure)
+
+  if (measure == "cpih") {
+    params <- list(sheet = "Table 11",
+                   rowmin = 7,
+                   rowmax = 371,
+                   colmin = 1,
+                   exclude = c("L5DD", "L5DE", "L5F3",
+                               "L5F4", "L5FP", "J4XP",
+                               "L5G8")
+                   )
+  } else if (measure == "cpi") {
+    params <- list(sheet = "Table 25",
+                   rowmin = 6,
+                   rowmax = 366,
+                   colmin = 1,
+                   exclude = c("ICVH", "ICVI", "CJXN",
+                               "CJXO", "CJYB", "L8LF")
+                   )
+  } else {
+    stop("measure must be 'cpi' or 'cpih'")
+  }
+
+  # dowmload the reference table spreadsheet
+  url <- reftables_url()
+
+  tmp <- tempfile()
+  utils::download.file(url, tmp)
+
+  janweights <- tidyxl::xlsx_cells(tmp, sheets = params$sheet) |>
+    dplyr::filter(row >= params$rowmin & row < params$rowmax & col > params$colmin) |>
+    unpivotr::behead("up", "year") |>
+    unpivotr::behead("left", "cdid") |>
+    unpivotr::behead("left", "title")  |>
+    dplyr::filter(!is.na(numeric)) |>
+    dplyr::filter(stringr::str_detect(.data$year, "Jan") == TRUE) |>
+    dplyr::mutate(date = lubridate::ym(.data$year),
+                  cdid = stringr::str_trim(.data$cdid)) |>
+    dplyr::select(date, .data$cdid, value = numeric) |>
+    dplyr::filter(!.data$cdid %in% params$exclude) |>
+    unique()
+
+  return(janweights)
+
+
+}
+
+
+
+#' Create a weights dataset
+#'
+#' @param rawfile A downloaded mm23.csv file. If missing will attempt to
+#'   download from ONS.
+#' @param measure The inflation figure to get weights for. Defaults to "CPI",
+#'   can be "CPIH"
+#'
+#' @return A dataframe of monthly weights, including the January weights which
+#'   are not exposed in mm23 but only available in the reference tables
+#'   spreadsheet.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' weights <- get_weights()
+#' cpih_weights <- get_weights(measure = "cpih")
+#' }
+get_weights <- function(rawfile, measure = "cpi") {
+
+  measure <- tolower(measure)
+  if (measure == "cpi") {
+    cat = "CPI Weights"
+  } else if (measure == "cpih") {
+    cat = "CPIH Weights"
+  } else {
+    stop("measure must be 'cpi' or 'cpih'")
+  }
+
+  if(!missing(rawfile)){
+    if(!file.exists(rawfile)) stop(paste(rawfile, "does not exist"))
+  }
+
+  if(missing(rawfile)){
+    mm23 <- acquire_mm23()
+  } else {
+    mm23 <- rawfile
+  }
+
+  metadata <- get_mm23_metadata(mm23)
+  yr <- get_mm23_year(mm23)
+  janweights <- get_weights_jan(measure = measure)
+  maxdate <- lubridate::floor_date(
+    lubridate::`%m-%`(lubridate::dmy(metadata$release_date[1]), months(1)),
+    unit = "months") |>
+    as.character()
+
+  wts <- metadata |>
+    dplyr::filter(.data$category == cat) |>
+    dplyr::select(.data$cdid) |>
+    unlist()
+
+  make_weight_series <- function(annual_data,
+                                 series,
+                                 start = "1998-01-01",
+                                 end = maxdate) {
+    data <- annual_data |>
+      dplyr::filter(.data$cdid == series)
+    dates <- data.frame(date = (seq.Date(as.Date(start), as.Date(end),by = "month")))
+    weights <- dates |>
+      dplyr::left_join(data, by = dplyr::join_by(date)) |>
+      tidyr::fill(.data$cdid:.data$period, .direction = "down")
+
+    return(weights)
+  }
+
+  weights <- purrr::map(wts, make_weight_series, annual_data = yr, .progress = "Getting weights") |>
+    purrr::list_rbind() |>
+    dplyr::select(-.data$period)
+
+  final_weights <- dplyr::rows_update(weights, janweights, by = c("date", "cdid"))
+
+  return(final_weights)
+}
+
+
+
+
 
 get_cpih_weights_jan <- function() {
   url <- reftables_url()
@@ -38,20 +170,6 @@ get_cpih_weights_jan <- function() {
 
 
 
-#' Create a CPIH weights dataset
-#'
-#' @param rawfile A downloaded mm23.csv file. If missing will attempt to
-#'   download from ONS.
-#'
-#' @return A dataframe of monthly CPIH weights, including the January weights
-#'   which are not exposed in mm23 but only available in the Table 11 sheet of
-#'   the reference tables spreadsheet.
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#' weights <- get_cpih_weights()
-#' }
 get_cpih_weights <- function(rawfile) {
 
   if(!missing(rawfile)){
@@ -168,21 +286,6 @@ get_cpi_weights_jan <- function() {
 
 
 
-#' Create a CPI weights dataset
-#'
-#' @param rawfile A downloaded mm23.csv file. If missing will attempt to
-#'   download from ONS.
-#'
-#'
-#' @return A dataframe of monthly CPIH weights, including the January weights
-#'   which are not exposed in mm23 but only available in the Table 11 sheet of
-#'   the reference tables spreadsheet.
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#' weights <- get_cpi_weights()
-#' }
 get_cpi_weights <- function(rawfile) {
 
   if(!missing(rawfile)){
