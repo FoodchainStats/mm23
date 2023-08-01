ae6f53a529227308154bec4463a42cc06c080218
David Lee <david@lee269.co.uk>
2023-08-01 21:56:37 +0100

    more weights tests

--- tests/testthat/test-weights.R
+++ tests/testthat/test-weights.R
@@ -1,3 +1,28 @@
+test_that("get_weights_jan returns a dataframe with positive length for CPI", {
+  skip_on_cran()
+  # skip_on_os("windows")
+
+  df <- get_weights_jan(measure = "cpi")
+
+  expect_equal(class(df), c("tbl_df", "tbl", "data.frame"))
+  expect_true(nrow(df) > 0)
+  expect_true(ncol(df) == 3)
+})
+
+
+test_that("get_weights_jan returns a dataframe with positive length for CPIH", {
+  skip_on_cran()
+  # skip_on_os("windows")
+
+  df <- get_weights_jan(measure = "cpih")
+
+  expect_equal(class(df), c("tbl_df", "tbl", "data.frame"))
+  expect_true(nrow(df) > 0)
+  expect_true(ncol(df) == 3)
+})
+
+
+
 test_that("get_weights returns a dataframe with positive length for CPI", {
   skip_on_cran()
   # skip_on_os("windows")
