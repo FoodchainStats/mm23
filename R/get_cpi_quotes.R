80a5028cc306c9cfb51ceb43535aa9d53ff20eeb
David Lee <david@lee269.co.uk>
2023-11-25 13:18:19 +0000

    update price quotes function

--- R/get_cpi_quotes.R
+++ R/get_cpi_quotes.R
@@ -33,7 +33,30 @@ read_quotes <- function(year, month) {
         )
       }
 
-  data <- readr::read_csv(url)
+  colspec <- readr::cols(
+    QUOTE_DATE = readr::col_double(),
+    ITEM_ID = readr::col_double(),
+    ITEM_DESC = readr::col_character(),
+    VALIDITY = readr::col_double(),
+    SHOP_CODE = readr::col_double(),
+    PRICE = readr::col_double(),
+    INDICATOR_BOX = readr::col_character(),
+    ORIG_INDICATOR_BOX = readr::col_character(),
+    PRICE_RELATIVE = readr::col_double(),
+    LOG_PRICE_RELATIVE = readr::col_double(),
+    STRATUM_WEIGHT = readr::col_double(),
+    STRATUM_TYPE = readr::col_double(),
+    START_DATE = readr::col_double(),
+    END_DATE = readr::col_double(),
+    REGION = readr::col_double(),
+    SHOP_TYPE = readr::col_double(),
+    SHOP_WEIGHT = readr::col_double(),
+    BASE_PRICE = readr::col_double(),
+    BASE_VALIDITY = readr::col_double(),
+    STRATUM_CELL = readr::col_double()
+  )
+
+  data <- readr::read_csv(url, col_types = colspec)
 
   data <- data |>
     janitor::clean_names() |>
@@ -57,23 +80,39 @@ read_quotes <- function(year, month) {
 
 
 
-#' Creates a dataframe of CPI price quotes for food items.
+#' Get CPI price quotes for food items.
 #'
 #' @param year Year to extract
 #' @param month Month to extract
-#' @param path path to save data
+#' @param path Optional path to save data
 #'
-#' @return a data frame of price quotes
+#' @return a tibble of price quotes
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' get_cpi_price_quotes(2021, 8)
 #' }
-get_cpi_price_quotes <- function(year, month, path = getwd()){
-  purrr::map2(year, month, \(year, month){
-    x <- read_quotes(year, month)
-    saveRDS(x, paste0(path, "/", year, "_", month, ".rds"))
-  })
+get_cpi_price_quotes <- function(year, month, path){
+
+  if(min(year) <= 2019) {stop("Does not work with dates before 2020 - use the make_archive function to get all 2010-2019 data")}
+
+  year1 <- rep(year, length(month)) |> sort()
+  month1 <- rep(month, length(year))
+
+  if(missing(path)){
+    purrr::map2(year1, month1, \(year1, month1){
+      x <- read_quotes(year1, month1)
+    }) |> purrr::list_rbind()
+
+  } else {
+  purrr::map2(year1, month1, \(year1, month1){
+    x <- read_quotes(year1, month1)
+    filename <- paste0(path, "/", year1, "-", formatC(month1, flag = "0", width = 2), "-price-quotes.rds")
+    message(paste("Saving", filename))
+    saveRDS(x, filename)
+  }) |> purrr::list_c()
+
+  }
 }
 
