34a4752df5f2dd45d0a0704ed8afd08995af19f2
David Lee <david.lee@defra.gov.uk>
2025-05-27 15:54:16 +0100

    added cpi quotes test

---
+++ tests/testthat/test-get_cpi_quotes.R
@@ -0,0 +1,8 @@
+test_that("get_cpi_price_quotes returns a dataframe with positive length", {
+  skip_on_cran()
+
+  df <- get_cpi_price_quotes(2022, 4)
+
+  expect_equal(class(df), c("tbl_df", "tbl", "data.frame"))
+  expect_true(nrow(df) > 0)
+})
