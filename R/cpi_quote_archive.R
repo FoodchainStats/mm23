0a02349b6b634b766497cd2d6c7a82ac75cb0f60
David Lee <david@lee269.co.uk>
2025-06-01 15:43:24 +0100

    price quote archive functions finished (I think)

--- R/cpi_quote_archive.R
+++ R/cpi_quote_archive.R
@@ -1,11 +1,176 @@
+
+#' Dataset of archive price quote urls
+#'
+#' Contains the url endings for all the price quote downloads for 2010-2019.
+#' Each url stub needs to be appended to the base
+#' "https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceindicescpiandretailpricesindexrpiitemindicesandpricequotes/"
+#'
+#' @returns A dataframe of yeqars and url stubs
+#' @export
+#'
+#' @examples
+#' archive_urls()
+archive_urls <- function() {
+  stub <- c(
+    "pricequotes2010/pricequote2010.zip",
+    "pricequotes2011/pricequote2011.zip",
+    "pricequotes2012/pricequote2012.zip",
+    "pricesquote2013/pricequote2013.zip",
+    "pricequotes2014/pricequote2014.zip",
+    "pricequotesquarter12015/upload-pricequote2015q1.csv",
+    "pricequotesquarter22015/upload-pricequote2015q2.csv",
+    "pricequotesquarter32015/upload-pricequote2015q3.csv",
+    "pricequotequarter42015/upload-pricequote2015q4.csv",
+    "pricequotequarter12016/upload-pricequote2016q1.csv",
+    "pricequotequarter22016/upload-pricequote2016q2.csv",
+    "pricequotequarter32016/upload-pricequote2016q3.csv",
+    "pricequotesquarter42016/upload-pricequote2016q4.csv",
+    "pricequotes2017/pricequote201703to201711.zip",
+    "pricequotesdecember2017/pricequote201712.zip",
+    "pricequotejanuary2018/pricequote201801.zip",
+    "pricequotefebruary2018/pricequote201802.zip",
+    "pricequotemarch2018/pricequote201803.zip",
+    "pricequoteapril2018/pricequote201804.zip",
+    "pricequotemay2018/pricequote201805.zip",
+    "pricequotejune2018/pricequote201806.zip",
+    "pricequotejuly2018/pricequote201807.zip",
+    "pricequoteaugust2018/pricequote201808.zip",
+    "pricequoteseptember2018/pricequote201809.zip",
+    "pricequoteoctober2018/pricequote201810.zip",
+    "pricequotenovember2018/pricequote201811.zip",
+    "pricequotesdecember2018/upload-pricequotes201812.csv",
+    "pricequotes2019/previous/v1/upload-pricequotes201901.csv",
+    "pricequotes2019/previous/v2/upload-pricequotes201902.csv",
+    "pricequotes2019/previous/v3/upload-pricequotes201903.csv",
+    "pricequotes2019/previous/v4/upload-pricequote201904.csv",
+    "pricequotes2019/previous/v5/upload-pricequote201905.csv",
+    "pricequotes2019/previous/v6/upload-pricequotes201906.csv",
+    "pricequotes2019/previous/v7/upload-pricequotes201907.csv",
+    "pricequotes2019/upload-pricequotes201908.csv",
+    "pricequotesseptember2019/upload-pricequotes201909.csv",
+    "pricequotesoctober2019/upload-pricequotes201910.csv",
+    "pricequotesnovember2019/upload-pricequotes201911.csv",
+    "pricequotesdecember2019/upload-pricequotes201912v1.csv"
+  )
+
+  yr <- c(2010,
+          2011,
+          2012,
+          2013,
+          2014,
+          2015, 2015, 2015, 2015,
+          2016, 2016, 2016, 2016,
+          2017, 2017,
+          2018, 2018, 2018, 2018, 2018, 2018, 2018, 2018, 2018, 2018, 2018, 2018,
+          2019, 2019, 2019, 2019, 2019, 2019, 2019, 2019, 2019, 2019, 2019, 2019)
+
+  x <- data.frame(yr, stub)
+  return(x)
+
+}
+
+
+#' Get old price quotes from 2010-2019
+#'
+#' @param year the year(s) to get
+#' @param foodonly whether to exclude non-food products from the output.
+#'
+#' @returns a dataframe of price quotes
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_cpi_price_quotes_archive(2015:2019)
+#' }
+get_cpi_price_quotes_archive <- function(year, foodonly = TRUE) {
+
+  if(max(year) >=2020 | min(year) <= 2009) {stop("Year must be in the range 2010-2019")}
+
+  tmpdir <- tempdir()
+  base <- "https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceindicescpiandretailpricesindexrpiitemindicesandpricequotes/"
+
+  stubs <- archive_urls() |> dplyr::filter(yr %in% year) |> dplyr::select(stub) |> as.list()
+  urls <- purrr::map2(base, stubs, paste0) |> unlist()
+  files <- basename(urls)
+
+  purrr::walk2(urls, files, \(urls,files){
+    acquire_safe(urls,paste0(tmpdir, "/",files))
+  })
+
+  list.files(tmpdir,pattern = "*.zip", full.names = TRUE) |>
+    purrr::walk(\(x) {
+      utils::unzip(x, exdir = tmpdir)
+      unlink(x)
+      })
+
+  # double unzip in 2017
+  list.files(tmpdir,pattern = ".*2017.*\\.zip", full.names = TRUE) |>
+    purrr::walk(\(x) {
+      utils::unzip(x, exdir = tmpdir)
+      unlink(x)
+      })
+
+  # Build dataset
+  output <- purrr::map(list.files(tmpdir, pattern = "*.csv", full.names = TRUE), \(x) {
+    message(paste("Reading", basename(x)))
+    data <- readr::read_csv(x, show_col_types = FALSE)
+
+    if(foodonly == TRUE) {
+      data <- data |>
+        janitor::clean_names() |>
+        dplyr::filter(item_id <= 320000 & item_id >= 210000) |>
+        dplyr::mutate(quote_date = lubridate::ym(.data$quote_date)) |>
+        dplyr::mutate(cs_id = NA,
+                      cs_desc = NA)
+    } else {
+      data <- data |>
+        janitor::clean_names() |>
+        dplyr::mutate(quote_date = lubridate::ym(.data$quote_date)) |>
+        dplyr::mutate(cs_id = NA,
+                      cs_desc = NA)
+    }
+
+    if(! rlang::has_name(data, "item_desc")) {
+      data <- data |> dplyr::mutate(item_desc = NA)
+    }
+    # message(paste("item_desc exists:", rlang::has_name(data, "item_desc")))
+
+    return(data)
+
+  }) |>
+    dplyr::bind_rows() |>
+    dplyr::select(.data$cs_id,
+                  .data$cs_desc,
+                  .data$quote_date,
+                  .data$item_id,
+                  .data$item_desc,
+                  .data$validity,
+                  .data$shop_code,
+                  .data$price,
+                  .data$indicator_box,
+                  .data$region,
+                  .data$shop_type,
+                  .data$shop_weight)
+
+
+  purrr::map(list.files(tmpdir, pattern = "*.csv", full.names = TRUE), unlink)
+  return(output)
+
+}
+
+
+
 #' Create an rds containing 2010-2019 CPI price quotes.
+#'
+#' This is older, more 'hardcoded' and was crashing on some computers with
+#' limited memory. The elegantly named get_cpi_price_quotes_archive() will do
+#' the same thing but for specific years.
 #'
 #' @param path The folder to put the final archive into.
 #' @param foodonly whether to exclude non-food products from the output. Can
 #'   help if your computer has memory issues with compiling the full dataset.
 #'
 #' @return creates an rds file in the chosen folder
-#' @export
 #'
 #' @examples
 #' \dontrun{
@@ -185,12 +350,12 @@ make_cpi_quote_archive <- function(path, foodonly = TRUE) {
     data <- readr::read_csv(x, show_col_types = FALSE)
 
     if(foodonly == TRUE) {
-    data <- data |>
-      janitor::clean_names() |>
-      dplyr::filter(item_id <= 320000 & item_id >= 210000) |>
-      dplyr::mutate(quote_date = lubridate::ym(.data$quote_date)) |>
-      dplyr::mutate(cs_id = NA,
-                    cs_desc = NA)
+      data <- data |>
+        janitor::clean_names() |>
+        dplyr::filter(item_id <= 320000 & item_id >= 210000) |>
+        dplyr::mutate(quote_date = lubridate::ym(.data$quote_date)) |>
+        dplyr::mutate(cs_id = NA,
+                      cs_desc = NA)
     } else {
       data <- data |>
         janitor::clean_names() |>
@@ -232,145 +397,3 @@ make_cpi_quote_archive <- function(path, foodonly = TRUE) {
   unlink(list.files(tmpdir, pattern = "*.csv", full.names = TRUE))
 
 }
-
-
-
-
-archive_stubs <- function() {
-  stub <- c(
-    "pricequotes2010/pricequote2010.zip",
-    "pricequotes2011/pricequote2011.zip",
-    "pricequotes2012/pricequote2012.zip",
-    "pricesquote2013/pricequote2013.zip",
-    "pricequotes2014/pricequote2014.zip",
-    "pricequotesquarter12015/upload-pricequote2015q1.csv",
-    "pricequotesquarter22015/upload-pricequote2015q2.csv",
-    "pricequotesquarter32015/upload-pricequote2015q3.csv",
-    "pricequotequarter42015/upload-pricequote2015q4.csv",
-    "pricequotequarter12016/upload-pricequote2016q1.csv",
-    "pricequotequarter22016/upload-pricequote2016q2.csv",
-    "pricequotequarter32016/upload-pricequote2016q3.csv",
-    "pricequotesquarter42016/upload-pricequote2016q4.csv",
-    "pricequotes2017/pricequote201703to201711.zip",
-    "pricequotesdecember2017/pricequote201712.zip",
-    "pricequotejanuary2018/pricequote201801.zip",
-    "pricequotefebruary2018/pricequote201802.zip",
-    "pricequotemarch2018/pricequote201803.zip",
-    "pricequoteapril2018/pricequote201804.zip",
-    "pricequotemay2018/pricequote201805.zip",
-    "pricequotejune2018/pricequote201806.zip",
-    "pricequotejuly2018/pricequote201807.zip",
-    "pricequoteaugust2018/pricequote201808.zip",
-    "pricequoteseptember2018/pricequote201809.zip",
-    "pricequoteoctober2018/pricequote201810.zip",
-    "pricequotenovember2018/pricequote201811.zip",
-    "pricequotesdecember2018/upload-pricequotes201812.csv",
-    "pricequotes2019/previous/v1/upload-pricequotes201901.csv",
-    "pricequotes2019/previous/v2/upload-pricequotes201902.csv",
-    "pricequotes2019/previous/v3/upload-pricequotes201903.csv",
-    "pricequotes2019/previous/v4/upload-pricequote201904.csv",
-    "pricequotes2019/previous/v5/upload-pricequote201905.csv",
-    "pricequotes2019/previous/v6/upload-pricequotes201906.csv",
-    "pricequotes2019/previous/v7/upload-pricequotes201907.csv",
-    "pricequotes2019/upload-pricequotes201908.csv",
-    "pricequotesseptember2019/upload-pricequotes201909.csv",
-    "pricequotesoctober2019/upload-pricequotes201910.csv",
-    "pricequotesnovember2019/upload-pricequotes201911.csv",
-    "pricequotesdecember2019/upload-pricequotes201912v1.csv"
-  )
-
-  yr <- c(2010,
-          2011,
-          2012,
-          2013,
-          2014,
-          2015, 2015, 2015, 2015,
-          2016, 2016, 2016, 2016,
-          2017, 2017,
-          2018, 2018, 2018, 2018, 2018, 2018, 2018, 2018, 2018, 2018, 2018, 2018,
-          2019, 2019, 2019, 2019, 2019, 2019, 2019, 2019, 2019, 2019, 2019, 2019)
-
-  x <- data.frame(yr, stub)
-  return(x)
-
-}
-
-
-archive <- function(year, path, foodonly = TRUE) {
-
-  if(!missing(path)) {
-    if(!dir.exists(path)) stop(paste(path, "does not exist"))
-  }
-
-  tmpdir <- tempdir()
-  base <- "https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceindicescpiandretailpricesindexrpiitemindicesandpricequotes/"
-
-  stubs <- archive_stubs() |> dplyr::filter(yr %in% year) |> dplyr::select(stub) |> as.list()
-  urls <- purrr::map2(base, stubs, paste0) |> unlist()
-  files <- basename(urls)
-
-  purrr::walk2(urls, files, \(urls,files){
-    acquire_safe(urls,paste0(tmpdir, "/",files))
-  })
-
-  list.files(tmpdir,pattern = "*.zip", full.names = TRUE) |>
-    purrr::walk(\(x) {
-      utils::unzip(x, exdir = tmpdir)
-      unlink(x)
-      })
-
-  # double unzip in 2017
-  list.files(tmpdir,pattern = ".*2017.*\\.zip", full.names = TRUE) |>
-    purrr::walk(\(x) {
-      utils::unzip(x, exdir = tmpdir)
-      unlink(x)
-      })
-
-  # Build dataset
-  output <- purrr::map(list.files(tmpdir, pattern = "*.csv", full.names = TRUE), \(x) {
-    message(paste("Reading", basename(x)))
-    data <- readr::read_csv(x, show_col_types = FALSE)
-
-    if(foodonly == TRUE) {
-      data <- data |>
-        janitor::clean_names() |>
-        dplyr::filter(item_id <= 320000 & item_id >= 210000) |>
-        dplyr::mutate(quote_date = lubridate::ym(.data$quote_date)) |>
-        dplyr::mutate(cs_id = NA,
-                      cs_desc = NA)
-    } else {
-      data <- data |>
-        janitor::clean_names() |>
-        dplyr::mutate(quote_date = lubridate::ym(.data$quote_date)) |>
-        dplyr::mutate(cs_id = NA,
-                      cs_desc = NA)
-    }
-
-    if(! rlang::has_name(data, "item_desc")) {
-      data <- data |> dplyr::mutate(item_desc = NA)
-    }
-    # message(paste("item_desc exists:", rlang::has_name(data, "item_desc")))
-
-    return(data)
-
-  }) |>
-    dplyr::bind_rows() |>
-    dplyr::select(.data$cs_id,
-                  .data$cs_desc,
-                  .data$quote_date,
-                  .data$item_id,
-                  .data$item_desc,
-                  .data$validity,
-                  .data$shop_code,
-                  .data$price,
-                  .data$indicator_box,
-                  .data$region,
-                  .data$shop_type,
-                  .data$shop_weight)
-
-
-  purrr::map(list.files(tmpdir, pattern = "*.csv", full.names = TRUE), unlink)
-  return(output)
-
-}
-
